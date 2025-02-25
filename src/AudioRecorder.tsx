0c7cacdb44310de593a37059e58a07b2f265deec
michaelp <michaelp@linux-laptop.lan>
2025-02-25 12:55:44 +0000

    Fixed timer Improved recorder

--- src/AudioRecorder.tsx
+++ src/AudioRecorder.tsx
@@ -1,122 +1,116 @@
-import { useState, useRef } from "react";
+import { useState, useRef,useEffect } from "react";
 import { Button } from "@aws-amplify/ui-react";
 const mimeType = "audio/webm";
 
 const AudioRecorder = () => {
-  const [permission, setPermission] = useState<boolean>(false);
+
   const mediaRecorder = useRef<MediaRecorder | null>(null);
+
+  const data = useRef<Blob[]>([]);
 
   const [recordingStatus, setRecordingStatus] = useState<
     "inactive" | "recording"
   >("inactive");
 
-  const [audioUrls, setAudioUrls] = useState<string[]>([]);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
 
 
-  const getMicrophonePermission = async () => {
-    if ("MediaRecorder" in window) {
-      try {
-        const mediaStream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: false,
-        });
-        mediaRecorder.current = new MediaRecorder(mediaStream, { mimeType });
-        mediaRecorder.current.ondataavailable = ondataavailableHandler ;
-        setPermission(true);
-        console.log("currentRecorder set ", mediaRecorder.current);
-      } catch (err) {
-        if (err instanceof Error) {
-          alert(err.message);
-        }
-      }
-    } else {
-      alert("The MediaRecorder API is not supported in your browser.");
-    }
-  };
+  const [count, setCount] = useState(0);
+
+    useEffect(() => {
+       
+        const interval = setInterval(() => {
+              setCount(count === 3 ? 0 :count +1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [count]);
 
 
-  const ondataavailableHandler = (event: BlobEvent) => {
-    console.log("data available event",event);
 
-    if (event.data.size > 0) {
-      const audioBlob = new Blob([event.data], { type: mimeType });
-      const audioUrl = window.URL.createObjectURL(audioBlob);
-      setAudioUrls((audioUrls) => [...audioUrls,audioUrl]);
-      console.log("blob created", audioUrls, audioUrl);
-    
-    }
-  };
- 
-  const startRecording = () => {
+  const startRecording = async () => {
     console.log("start recording running ...");
-    
-    if (mediaRecorder.current) {
-      mediaRecorder.current.start(3000);
+
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+
+      mediaRecorder.current = new MediaRecorder(mediaStream, { mimeType });
+
+      mediaRecorder.current.ondataavailable = (event: BlobEvent) => {
+        console.log("data available event", event);
+        if (event.data.size > 0) {
+          data.current.push(event.data);
+        }
+      };
+
+      mediaRecorder.current.onstop = () => {
+        const audioBlob = new Blob(data.current, { type: mimeType });
+        const audioUrl = window.URL.createObjectURL(audioBlob);
+        setAudioUrl(audioUrl);
+      };
+
+      mediaRecorder.current.start();
       setRecordingStatus("recording");
+    } catch (err) {
+      if (err instanceof Error) {
+        alert(err.message);
+      }
     }
   };
 
   const stopRecording = () => {
-    if (!mediaRecorder.current) return;
-    setRecordingStatus("inactive");
-    mediaRecorder.current.stop();
-    
+    if (mediaRecorder.current && mediaRecorder.current.state === "recording") {
+      mediaRecorder.current.stop();
+      setRecordingStatus("inactive");
+    }
+  };
+
+  const ShowRecordStatusButton = () => {
+    if (recordingStatus === "inactive") {
+      return (
+        <Button
+          onClick={startRecording}
+          type="button"
+          variation="primary"
+          colorTheme="overlay"
+        >
+          Start Recording
+        </Button>
+      );
+    }
+
+    return (
+      <>
+        <Button
+          onClick={stopRecording}
+          type="button"
+          variation="primary"
+          colorTheme="overlay"
+        >
+          Stop Recording
+        </Button>
+          <span> {recordingStatus}{".".repeat(count)}</span>
+     
+      </>
+    );
   };
 
   return (
     <>
       <div className="audio-controls">
-        {!permission && (
-          <Button
-            onClick={getMicrophonePermission}
-            type="button"
-            variation="primary"
-            colorTheme="overlay"
-          >
-            Get Microphone
-          </Button>
-        )}
-
-        {permission && recordingStatus === "inactive" && (
-          <Button
-            onClick={startRecording}
-            type="button"
-            variation="primary"
-            colorTheme="overlay"
-          >
-            Start Recording
-          </Button>
-        )}
-
-        {recordingStatus === "recording" && (
-          <>
-            <Button
-              onClick={stopRecording}
-              type="button"
-              variation="primary"
-              colorTheme="overlay"
-            >
-              Stop Recording
-            </Button>
-            <span> {recordingStatus} </span>
-          </>
-        )}
+        <ShowRecordStatusButton />
       </div>
-
-      <ol>
-        {audioUrls.map((url, i) => (
-          <li key={i}>
-            <div className="audio-player">
-              <br />
-              <audio src={url} controls></audio>
-              <br />
-              <a download href={url}>
-                Download Recording
-              </a>
-            </div>
-          </li>
-        ))}
-      </ol>
+      {audioUrl != null && (
+        <div className="audio-player">
+          <br />
+          <audio src={audioUrl} controls></audio>
+          <br />
+          <a download href={audioUrl}>
+            Download Recording
+          </a>
+        </div>
+      )}
     </>
   );
 };
